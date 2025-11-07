dffb4bff65a7bf2024ee8991c1dd5166c21cc5d8
merajhos <meraj357685@gmail.com>
2025-11-07 17:42:45 +0600

    added submit

--- greenNest/src/components/ProtectedRoute.jsx
+++ greenNest/src/components/ProtectedRoute.jsx
@@ -8,7 +8,7 @@ import Loading from "../pages/Loading";
   const { user, loading } = useContext(AuthContext);
   const location = useLocation();
 
-//   if (loading) return <Loading></Loading>;
+  if (loading) return <Loading></Loading>;
 
   if (!user) {
     return <Navigate to="/login" state={{ from: location }} replace />;
